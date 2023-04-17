a73ad6073b4926632675c1f2ac8f9579d9ef9d16
Oleksii Khomiakov <alex.xag1987@gmail.com>
2023-04-18 00:19:58 +0300

    parcel 18.04.2023

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import validatePassword from "./js/validate-password.js";
+// import apiServce from "./js/api-service.js";
+import {
+  fetchAllUsers,
+  fetchUserById,
+  updateUserById,
+} from "./js/api-service.js";
+
+// console.log(validatePassword);
+// console.log(validatePassword("qweqwe"));
+
+console.log(fetchAllUsers);
