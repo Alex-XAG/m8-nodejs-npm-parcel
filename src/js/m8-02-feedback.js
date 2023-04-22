bc34ea9a67936e7286175c3d01ce8d61f27c587a
Oleksii Khomiakov <alex.xag1987@gmail.com>
2023-04-22 20:44:30 +0300

    feedback

--- src/js/m8-02-feedback.js
+++ src/js/m8-02-feedback.js
@@ -1,6 +1,8 @@
 import throttle from "lodash.throttle";
 
-const STORAGEKEY = "feedback-msg";
+const STORAGE_KEY = "feedback-msg";
+
+const formData = {};
 
 const refs = {
   form: document.querySelector(".js-feedback-form"),
@@ -10,26 +12,35 @@ const refs = {
 refs.form.addEventListener("submit", onFormSubmit);
 refs.textarea.addEventListener("input", throttle(onTextareaInput, 500));
 
+// refs.form.addEventListener('input', e => {
+//   // console.log(e.target.name);
+//   // console.log(e.target.value);
+// formData[e.target.name] = e.target.value;
+
+// console.log(formData);
+
+// })
+
 populateTextarea();
 
 function onFormSubmit(evt) {
   evt.preventDefault();
 
   evt.currentTarget.reset();
-  localStorage.removeItem(STORAGEKEY);
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 // - Получаем значение поля
 // - Сохраняем его в хранилище
 // - Можно добавить throttle
 function onTextareaInput(evt) {
-  const message = evt.currentTarget.value;
+  const message = evt.target.value;
 
-  localStorage.setItem(STORAGEKEY, message);
+  localStorage.setItem(STORAGE_KEY, message);
 }
 
 function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGEKEY);
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
 
   if (savedMessage) {
     refs.textarea.value = savedMessage;
